3e0173764dd25a74e3dcafe584f05b6825f18a16
Kamil Adamuszek <kamil.adamuszek@makolab.com>
2018-07-01 12:10:46 +0200

    components, data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  newTask: string;
+  tasksLists: Array<string> = [];
+  tasksDone: Array<string> = [];
 
+  add() {
+    this.tasksLists.push(this.newTask);
+    this.newTask = '';
+  }
+
+  remove(task: string) {
+    this.tasksLists = this.tasksLists.filter(e => e !== task);
+  }
+
+  done(task: string) {
+    this.tasksDone.push(task);
+    this.remove(task);
+  }
 }
